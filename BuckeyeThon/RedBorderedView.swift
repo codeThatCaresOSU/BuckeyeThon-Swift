e42082221123eb8cdd97ba5a478b912ef10b8cfc
Taha Topiwala <taha.topiwala@icloud.com>
2016-11-18 22:00:17 -0500

    Initial set up for storyboards and master view controller

---
+++ BuckeyeThon/RedBorderedView.swift
@@ -0,0 +1,19 @@
+//
+//  RedBorderedView.swift
+//  BuckeyeThon
+//
+//  Created by Taha Topiwala on 11/17/16.
+//  Copyright © 2016 Taha Topiwala. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RedBorderedView: UIView {
+    
+    override func awakeFromNib() {
+        self.layer.borderColor = UIColor(displayP3Red: 187/255, green: 0, blue: 0, alpha: 1).cgColor
+        self.layer.borderWidth = 8
+        self.layer.cornerRadius = 2.0
+    }
+
+}
---
+++ BuckeyeThon/SmallRoundedButtons.swift
@@ -0,0 +1,16 @@
+//
+//  SmallRoundedButtons.swift
+//  BuckeyeThon
+//
+//  Created by Taha Topiwala on 11/17/16.
+//  Copyright © 2016 Taha Topiwala. All rights reserved.
+//
+
+import UIKit
+
+class SmallRoundedButtons: UIButton {
+
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 5.0
+    }
+}
