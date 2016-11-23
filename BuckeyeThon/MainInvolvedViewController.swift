bc289e8c7944d772cd1f23c412a09434771d8ef2
Taha Topiwala <taha.topiwala@icloud.com>
2016-11-22 19:15:44 -0500

    Involved
    Adds options for "Get Involved Options"

---
+++ BuckeyeThon/MainInvolvedViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainInvolvedViewController.swift
+//  BuckeyeThon
+//
+//  Created by Taha Topiwala on 11/22/16.
+//  Copyright © 2016 Taha Topiwala. All rights reserved.
+//
+
+import UIKit
+
+class MainInvolvedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.title = "Involved"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
