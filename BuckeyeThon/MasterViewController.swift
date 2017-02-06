22732b8e51421005114770dd39acd042b1f07d08
Ian Willis <ianwillis98@gmail.com>
2017-02-06 18:17:59 -0500

    working on banner and user location

--- BuckeyeThon/MasterViewController.swift
+++ BuckeyeThon/MasterViewController.swift
@@ -7,29 +7,52 @@
 //
 
 import UIKit
+import CoreLocation
 
-class MasterViewController: UIViewController {
 
+class MasterViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var banner: UIView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        //if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        //}
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    @IBAction func onButtonClick(_ sender: Any) {
+        banner.isHidden = !banner.isHidden
+        //locationManager.requestLocation()
     }
-    */
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("Ian  locations = \(locValue.latitude) \(locValue.longitude)")
+    }
 }
